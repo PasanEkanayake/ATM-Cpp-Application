d4f42c6ee82818595cf0f7a07b28b646c5a28f1f
Pasan Ekanayake <pasanekanayake@gmail.com>
2024-06-17 10:57:57 +0530

    commit

--- ATM-Cpp-Application/ATM-Cpp-Application.cpp
+++ ATM-Cpp-Application/ATM-Cpp-Application.cpp
@@ -12,11 +12,15 @@ int main()
 	int pinNumber;
 	int len = 1;
 	string userName;
-	cout << "Enter a New User Name: ";
+	cout << "\nInstruction: When entering your user name, follow this format - \nMr./Ms./Mrs./Miss/Dr./Rev. <space> first-Name <space> Middle-Name(optional) <space> Last-Name." << endl;
+	cout << "\nEnter a New User Name: ";
 	getline(cin >> ws, userName);
 
+
+
+	//Entering Pin Number and checking whether it was a 4 digit number.
 	do {
-		cout << "\nCreate a 4 digit pin number consist only with numbers (1,2,3,4,5,6,7,8,9,0)." << endl;
+		cout << "\nInstruction: Create a 4 digit pin number consist only with numbers (1,2,3,4,5,6,7,8,9,0)." << endl;
 		cout << "\nEnter a New Pin Number: ";
 		cin >> pinNumber;
 		int temp = pinNumber;
@@ -42,6 +46,7 @@ int main()
 
 	} while (len != 4);
 
+	//Entering Initial Depost amount and checking it's validity.
 	do {
 		cout << "You must have at least Rs.500/= or more initial deposit to open a new account." << endl;
 		cout << "\nYour Initial Deposit Amount(Rs.): ";
@@ -59,8 +64,29 @@ int main()
 		}
 	} while (approvedAccount < 1);
 
-	cout << endl << userName << ' ' << pinNumber << ' ' << initialDeposit;
+	/*cout << endl << userName << ' ' << pinNumber << ' ' << initialDeposit;*/
 
-	/*system("cls");*/
+	system("cls");
+
+	char goToATM;
+	cout << "Do you want to go to the ATM?" << endl;
+	cout << "If 'Yes', Enter 'Y' or 'y'" << endl;
+	cout << "If 'No', Enter 'N' or 'n'" << endl;
+	cout << "\nYour Response: ";
+	cin >> goToATM;
+
+	goToATM = toupper(goToATM);
+
+	if (goToATM == 'Y')
+	{
+
+	}
+	else
+	{
+		system("cls");
+		cout << "Thank you " << userName <<", for joining with us at the World Bank. Have a nice day.";
+	}
 	
+	system("pause>0");
+
 }
